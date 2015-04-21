b7682ab68fea889a1be7d13097c7ae3c732ec90f
Isabella Jorissen <ifjorissen@gmail.com>
2015-04-21 11:33:04 -0700

    pretty sure this will work for a candidate still need to update the elections model

--- elections/urls.py
+++ elections/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     'elections.views',
+    
     (r'^$', 'index'),
     (r'^vote/?$', 'vote'),
-    (r'^submit_vote/?$', 'submit_vote')
-)
+    (r'^submit_vote/?$', 'submit_vote'),
+		(r'^admin/$', 'admin_index'),
+    (r'^admin/create/$', 'create_election'),
+    (r'^admin/delete/(?P<identifier>\d{1,10})/$','delete_election'),
+    (r'^admin/deleted/$', 'deleted')
+   )

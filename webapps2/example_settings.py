b4d2b41a1999127f2e921e7b393e2e4520a9a8e6
Will Jones <thefreethinker127@gmail.com>
2015-04-23 01:32:37 -0700

    Removed secret key and made settings file into example. Future iterations will have to create their own copy and generate their own key.

--- webapps2/example_settings.py
+++ webapps2/example_settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zy7f&$-#s)k4o6&0@f6a57l7ou--u-4kag98hz4@@_nhpwxp%u'
+SECRET_KEY = 'wouldntyouliketoknow'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

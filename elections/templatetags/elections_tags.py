10ebba55037c422aacae6c7991153786d5d5cd0a
Will Jones <thefreethinker127@gmail.com>
2015-04-23 12:44:53 -0700

    Made votes populate in form

---
+++ elections/templatetags/elections_tags.py
@@ -0,0 +1,13 @@
+from django.template.defaulttags import register
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+@register.filter
+def is_false(arg): 
+    return arg is False
+
+@register.filter
+def is_not_false(arg): 
+    return arg is not False

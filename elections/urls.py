ea904abd8b24d5f3a4ab4864c1493f030765955a
Will Jones <willjones127@gmail.com>
2015-04-25 19:27:24 -0700

    Made urls have slashes. Also removed links to other apps temporarily. Will have those links return next semester, when we can start with a blank database.

--- elections/urls.py
+++ elections/urls.py
@@ -4,8 +4,8 @@ urlpatterns = patterns(
     'elections.views',
     
     (r'^$', 'index'),
-    (r'^vote/?$', 'vote'),
-    (r'^submit_vote/?$', 'submit_vote'),
+    (r'^vote/$', 'vote'),
+    (r'^submit_vote/$', 'submit_vote'),
 		# (r'^admin/$', 'admin_index')
     # (r'^admin/create/$', 'create_election')
     # (r'^admin/delete/(?P<identifier>\d{1,10})/$','delete_election'),

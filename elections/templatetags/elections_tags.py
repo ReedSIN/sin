cf4404438811ca298dda5da3afdbbc33dbd46d2e
Isabella Jorissen <ifjorissen@gmail.com>
2015-04-28 11:17:11 -0700

    fixed the elections tag bug

--- elections/templatetags/elections_tags.py
+++ elections/templatetags/elections_tags.py
@@ -2,12 +2,9 @@ from django.template.defaulttags import register
 
 @register.filter
 def get_item(dictionary, key):
-    x = dictionary.get(key)
-    if x is None:
-        return ''
-    else:
-        return x
-
+  if dictionary is not None:
+    return dictionary.get(key)
+    
 @register.filter
 def is_false(arg):
     return arg is False

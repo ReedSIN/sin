0ab9f3a7cd9241194e5294a5505ab0dcb311de4e
Will Jones <willjones127@gmail.com>
2016-01-24 14:34:07 -0800

    Use filter menu in admin to select users for a factor.

--- generic/admin.py
+++ generic/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from generic.models import *
 # Register your models here.
+
+class FactorAdmin(admin.ModelAdmin):
+    filter_horizontal = ('users',)
+
 admin.site.register(SinUser)
-admin.site.register(Factor)
+admin.site.register(Factor, FactorAdmin)
 admin.site.register(Organization)

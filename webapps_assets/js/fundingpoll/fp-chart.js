dc69449c1db4a1b05db1882126a949b14cf9a942
Will Jones <thefreethinker127@gmail.com>
2016-02-06 22:05:08 -0800

    Add additional chart versions and controls to change

--- webapps_assets/js/fundingpoll/fp-chart.js
+++ webapps_assets/js/fundingpoll/fp-chart.js
@@ -1,47 +1,61 @@
-var chart = d3.select('#top-chart');
+var fp_results_chart = (function() {
+    /* Initialize Chart */
+    var chart = d3.select('#top-chart');
 
-// Accessor functions
-function name(d) { return d.name; }
-function signator(d) { return d.signator; }
-function points(d) { return d.points; }
-function topsix(d) { return d.topsix; }
-function approve(d) { return d.approve; }
-function noopinion(d) { return d.noopinion; }
-function disapprove(d) { return d.disapprove; }
-function deepsix(d) { return d.deepsix; }
+    // Accessor functions
+    function name(d) { return d.name; }
+    function signator(d) { return d.signator; }
+    function points(d) { return d.points; }
+    function topsix(d) { return d.topsix; }
+    function approve(d) { return d.approve; }
+    function noopinion(d) { return d.noopinion; }
+    function disapprove(d) { return d.disapprove; }
+    function deepsix(d) { return d.deepsix; }
+    function dis_deep(d) { return disapprove(d) + deepsix(d); }
+    function app_top(d) { return approve(d) + topsix(d); }
 
-// Chart dimensions.
-var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
-    width = 900 - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    // Chart dimensions.
+    var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
+        width = 900 - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
+    // Create the SVG container and set the origin.
+    var svg = chart.append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    d3.json("/static/data/results_json.json", init_chart);
 
+    // We need these variables in this scope.
+    var orgs, xScale, yScale, radiusScale, colorScale, xAxis, yAxis;
 
-// Create the SVG container and set the origin.
-var svg = chart.append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    /* Functions! */
 
+    /**
+     * @function update_top_deep updates chart to show top sixes versus deep sixes
+     */
+    var update_app_dis = update_general(disapprove, approve, "disapprovals", "approvals"),
+        update_top_deep = update_general(deepsix, topsix, "deep sixes", "top sixes"),
+        update_hybrid = update_general(dis_deep, app_top, "disapprovals + deep sixes",
+                                      "approvals + top sixes");
 
-// Load the data.
-d3.json("/static/data/results_json.json", function(data) {
-    var orgs = data.fp_orgs;
+    function init_chart(data) {
+        orgs = data.fp_orgs;
 
-    // Various scales. These domains make assumptions of data, naturally.
-    var xScale = d3.scale.linear()
+        // Various scales. These domains make assumptions of data, naturally.
+        xScale = d3.scale.linear()
             .domain([0,d3.max(orgs.map(disapprove))])
             .range([0, width]),
         yScale = d3.scale.linear()
             .domain([0,d3.max(orgs.map(approve))])
             .range([height, 0]),
-        radiusScale = d3.scale.sqrt().domain([0,d3.max(orgs.map(points))]).range([0, 40]),
+        radiusScale = d3.scale.sqrt().domain(d3.extent(orgs.map(points))).range([0, 40]),
         colorScale = d3.scale.category10();
 
-    // The x & y axes.
-    var xAxis = d3.svg.axis().orient("bottom")
+        // The x & y axes.
+        xAxis = d3.svg.axis().orient("bottom")
             .scale(xScale).ticks(6, d3.format(",d"))
             .tickSize(-height,0)
             .tickPadding(10),
@@ -51,56 +65,127 @@ d3.json("/static/data/results_json.json", function(data) {
             .tickPadding(10,0);
 
 
-    // Add the x-axis.
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        // Add the x-axis.
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
-    // Add the y-axis.
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
+        // Add the y-axis.
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
 
-    // Add an x-axis label.
-    svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height - 6)
-        .text("disapprovals");
+        // Add an x-axis label.
+        svg.append("text")
+            .attr("class", "x label")
+            .attr("text-anchor", "end")
+            .attr("x", width)
+            .attr("y", height - 6)
+            .text("disapprovals");
 
-    // Add a y-axis label.
-    svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("y", 6)
-        .attr("dy", ".75em")
-        .attr("transform", "rotate(-90)")
-        .text("approvals");
+        // Add a y-axis label.
+        svg.append("text")
+            .attr("class", "y label")
+            .attr("text-anchor", "end")
+            .attr("y", 6)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text("approvals");
 
-    // Tooltips
-    var tip_template = _.template($('#template-org').html());
-    var makeTemplate = function (d) {
-        return tip_template({name: name(d),
-                            points: points(d),
-                            approve: approve(d),
-                            noopinion: noopinion(d),
-                            disapprove: disapprove(d),
-                            deepsix: deepsix(d)});
+        // Tooltips
+        var tip_template = _.template($('#template-org').html());
+        var makeTemplate = function (d) {
+            return tip_template({name: name(d),
+                                 points: points(d),
+                                 approve: approve(d),
+                                 noopinion: noopinion(d),
+                                 disapprove: disapprove(d),
+                                 deepsix: deepsix(d)});
+        };
+        var tip = d3.tip().attr('class', 'd3-tip').html(makeTemplate);
+        svg.call(tip);
+
+        svg.append('g')
+            .classed('circles', true)
+            .selectAll('circle')
+            .data(orgs)
+            .enter()
+            .append('circle')
+            .classed('org-circle', true)
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+            .attr('cx', sequence(disapprove, xScale))
+            .attr('cy', sequence(approve, yScale))
+            .attr('r', sequence(points, radiusScale));
+
+
+    }
+
+    function update_general(x_access, y_access, x_label, y_label) {
+        return function() {
+            // Update scales
+            xScale = d3.scale.linear()
+                .domain([0,d3.max(orgs.map(x_access))])
+                .range([0, width]);
+            yScale = d3.scale.linear()
+                .domain([0,d3.max(orgs.map(y_access))])
+                .range([height, 0]);
+
+            // Update Axes
+            svg.select(".y.axis")
+                .call(yAxis);
+            svg.select(".x.axis")
+                .call(xAxis);
+
+            // Update Axes Labels
+            svg.select(".x.label")
+                .text(x_label);
+            svg.select(".y.label")
+                .text(y_label);
+
+            // Update points
+            svg.selectAll('circle')
+                .transition()
+                .attr('cx', sequence(x_access, xScale))
+                .attr('cy', sequence(y_access, yScale));
+        };
+    }
+
+    return { update_app_dis: update_app_dis,
+             update_top_deep: update_top_deep,
+             update_hybrid: update_hybrid };
+})();
+
+
+// Add event handlers
+(function() {
+    var buttons = $('#plot-choices').children();
+
+    $('#app_dis').click(select_new_plot('app_dis', fp_results_chart.update_app_dis));
+    $('#top_deep').click(select_new_plot('top_deep', fp_results_chart.update_top_deep));
+    $('#hybrid').click(select_new_plot('hybrid', fp_results_chart.update_hybrid));
+
+    function select_new_plot(name, update_function) {
+        return function() {
+            buttons.removeClass('active');
+            $('#' + name).addClass('active');
+            update_function();
+            return false;
+        };
+    }
+})();
+
+
+/**
+ * @function sequence takes any number of functions and returns their left-to-right composition
+ */
+function sequence() {
+    var fns = arguments;
+    return function (result) {
+        for (var i = 0; i < fns.length; i++) {
+            result = fns[i].call(this, result);
+        }
+        return result;
     };
-    var tip = d3.tip().attr('class', 'd3-tip').html(makeTemplate);
-    svg.call(tip);
-
-    svg.append('g')
-        .selectAll('circle')
-        .data(orgs)
-        .enter()
-        .append('circle')
-        .attr('cx', function (d) { return xScale(disapprove(d)); })
-        .attr('cy', function (d) { return yScale(approve(d)); })
-        .attr('r', function (d) { return radiusScale(points(d)); })
-        .classed('org-circle', true)
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-});
+};
